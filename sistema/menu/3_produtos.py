65772d23f253e2a628478be6efb2ade9e6acda5f
BrendonJoaoAnalistadeSistema <brendonanalistadesistemas@gmail.com>
2024-07-09 19:54:41 -0300

    # 005 - produtos

---
+++ sistema/menu/3_produtos.py
@@ -0,0 +1,67 @@
+# Curso de Desenvolvimento de Sistemas.
+# Turma: 0152.
+# Autor: Brendon João Campos Neves.
+# Data início: 28/06/2024.
+# Data final: /06/2024.
+# Objetivo: Construir um "banco de dados" com os produtos da loja.
+
+# Categorias:
+tecnologia = []
+moda = []
+cuidados_pessoais = []
+entretenimento = []
+alimentos = []
+casa = []
+brinquedos = []
+esportes = []
+bem_estar = []
+ferramentas = []
+
+# Produtos:
+smartphone = {}
+notebooks = {}
+fones_ouvido = {}
+smartwatche = {}
+tablete = {}
+
+camisa_basica = {}
+calca_jeans = {}
+vestido = {}
+tenis = {}
+bolsa = {}
+
+hidratante = {}
+paleta_sombras = {}
+perfumes = {}
+escova_cabelo = {}
+kit_maquiagem = {}
+
+livro = {}
+album_musica = {}
+box_series = {}
+filmes = {}
+e_book = {}
+
+cafe_gourmet = {}
+vinho = {}
+chocolate = {}
+barra_cereal = {}
+especiarias = {}
+
+sofa_cama = {}
+mesa_jantar = {}
+cortinas_blackout = {}
+luminaria = {}
+quadro = {}
+
+bonecos_colecionaveis = {}
+triciclos = {}
+jogo_tabuleiro = {}
+pelucia = {}
+bloco_montar = {}
+
+bola_futebol = {}
+esteira = {}
+peso = {}
+roupa_esportiva = {}
+tenis_corrida = {}
